4735b5b3a9b7272aa144f5ba207649417d60020c
Сергій <kjserg@i.ua>
2025-09-22 16:59:38 +0300

    fix instagram section

--- resources/js/plugins/Slick.js
+++ resources/js/plugins/Slick.js
@@ -72,18 +72,21 @@ export default class Slick {
 
 
 function change_sbi_images_html() {
-    $(document).find('.instagram-posts').each(function () {
+
+    $(document).find('.instagram-posts').each(function (index) {
         const $wrap = $(this);
         let html = '';
         const $items = $wrap.find('.sbi_photo');
-        $items.each(function () {
+        $items.each(function (i) {
             const $item = $(this);
             const href = $item.attr('href');
             const src = $item.attr('data-full-res');
+            const imageElement = $item[0];
+            const ID = `instagram-post__image-${index}-${i}`;
             let temp = `
              <div>
                         <div class="instagram-post" >
-                            <a href="${href}" class="instagram-post__image" style="display:block;">
+                            <a href="${href}" id="${ID}" class="instagram-post__image" style="display:block;">
                                 <img src="${src}" class="cover" alt="">
                             </a>
                         </div>
@@ -96,4 +99,40 @@ function change_sbi_images_html() {
             $wrap.html(html);
         }
     });
+}
+
+function getImageSize(src) {
+    return new Promise((resolve, reject) => {
+        const img = new Image();
+
+        img.onload = () => {
+            // Promise успішно виконано, повертаємо об'єкт з розмірами
+            resolve({
+                width: img.width,
+                height: img.height
+            });
+        };
+
+        img.onerror = (error) => {
+            // Якщо сталася помилка, відхиляємо Promise
+            reject(`Помилка завантаження зображення: ${src}`);
+        };
+
+        img.src = src;
+    });
+}
+function setAspectRatioPadding($el, imageWidth, imageHeight) {
+    console.log($el)
+    console.log(imageWidth)
+
+    if ($el.length === 0) {
+        return;
+    }
+    if (imageWidth === 0) {
+        return;
+    }
+    const container = $el[0];
+    const paddingPercentage = (imageHeight / imageWidth) * 100;
+    container.style.paddingBottom = `${paddingPercentage}%`;
+    console.log(`Для контейнера встановлено padding-bottom: ${paddingPercentage}%`);
 }
